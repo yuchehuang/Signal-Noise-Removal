a213a55a6e14393db26b8969447a46467419c3f9
Yuche Huang <53272971+yuchehuang@users.noreply.github.com>
2019-12-06 22:10:16 +0800

    Rename the name of the variables delete the redundant code

--- code/Part_2_square_optimize.m
+++ code/Part_2_square_optimize.m
@@ -1,193 +1,122 @@
 clc
 close all
-%% ------------------Parameter-------------------
+%% ---------Parameter---------
 Fs=1000;
 Ts=1/Fs;
 Length=3000;
 t=(0:Length-1)*Ts;
 Excel(1:5,1:50)=0;
-%% ------------------Signal & NOise-------------------
+%% ---------Signal & NOise---------
 section_length=[0 704 1005 1709 2002 2296 ];
-y1=noise+signal;
+Mix=noise+signal;
 New=signal;
 
 Excel(5,100)=0;
-for segment=2:2  % set which section u want to compute
-    s=signal(section_length(segment)+1:section_length(segment+1));
+for segment=1:5  % set which section u want to compute
+    Original_segment_signal=signal(section_length(segment)+1:section_length(segment+1));
     N=noise(section_length(segment)+1:section_length(segment+1));
-    n2=y1(section_length(segment)+1:section_length(segment+1));
-    Size= size(s);
+    Mix_Signal=Mix(section_length(segment)+1:section_length(segment+1));
+    Size= size(Original_segment_signal,2);
     figure(segment);
     subplot(4,1,1);
-    plot(t(1:Size(2)),s);
+    plot(t(1:Size),Original_segment_signal);
     xlabel('time (s)'); 
     ylabel('voltage (mV)');
     title('Original Signal');
     
-%%  plot signal+noise figure    
+%%  ---------plot signal+noise figure---------    
     subplot(4,1,2);
-    plot(t(1:Size(2)),n2)      %x unit ms
-    Length=Size(2);
+    plot(t(1:Size),Mix_Signal)      %x unit ms
+    Length=Size;
     xlabel('time (s)'); 
     ylabel('voltage (mV)');
-    txt=sprintf('Noise Signal   PSNR= %f dB', psnr(s,n2,255));
+    txt=sprintf('Noise Signal   PSNR= %f dB', psnr(Original_segment_signal,Mix_Signal,255));
     title(txt);
     
-%% ------(Original) Frequency Domain------------------------------- 
+%% ---------(Original) Frequency Domain--------- 
     NFFT = 2^nextpow2(Length);
-    Y=fft(s,NFFT);
-    P2 = abs(Y/Length);
-    P1 = P2(1:NFFT/2+1);
-    P1(2:end-1) = 2*P1(2:end-1);
+    FFT_output=fft(Original_segment_signal,NFFT);
+    FFT_Temp_2 = abs(FFT_output/Length);
+    FFT_Temp_1 = FFT_Temp_2(1:NFFT/2+1);
+    FFT_Temp_1(2:end-1) = 2*FFT_Temp_1(2:end-1);
     subplot(4,1,3);
     hold on
-    plot(0:(1/NFFT):(1/2-1/NFFT),P1(1:NFFT/2))   %0:(Fs/NFFT):(Fs/2-Fs/NFFT)-- not normalization
+    plot(0:(1/NFFT):(1/2-1/NFFT),FFT_Temp_1(1:NFFT/2))   %0:(Fs/NFFT):(Fs/2-Fs/NFFT)-- not normalization
     xlabel('Normalised Frequency (f)'); 
     ylabel('voltage (mV)');
     title('Frequency Spectrum');    
     
-%% ---------(With Noise) Frequency Domain------------ 
-
+%% ---------(With Noise) Frequency Domain--------- 
     NFFT = 2^nextpow2(Length);
-    Y=fft(n2,NFFT);
-    P2 = abs(Y/Length);
-    P1 = P2(1:NFFT/2+1);
-    P1(2:end-1) = 2*P1(2:end-1);
-    plot(0:(1/NFFT):(1/2-1/NFFT),P1(1:NFFT/2),'R')
-    FFT_value=P1(1:NFFT/2);
+    FFT_output=fft(Mix_Signal,NFFT);
+    FFT_Temp_2 = abs(FFT_output/Length);
+    FFT_Temp_1 = FFT_Temp_2(1:NFFT/2+1);
+    FFT_Temp_1(2:end-1) = 2*FFT_Temp_1(2:end-1);
+    plot(0:(1/NFFT):(1/2-1/NFFT),FFT_Temp_1(1:NFFT/2),'R')
+    FFT_value=FFT_Temp_1(1:NFFT/2);
     
+%% ---------Denoise Operation---------
     
- 
- %% --------- Denoise Operation---------------------
-    %threshold  = mean(abs(Y));
-    %threshold = thselect(n2,'rigrsure')
-    Ysize=size(Y);
-    MAX=0;
-    %--------- Optimize the parameter---------------------    
-%     for paramteter=1:1000
-%         Y_temp=Y;
-%         for i=1:Ysize(2)
-%             if abs(Y_temp(i)) < paramteter
-%                 Y_temp(i) = 0;
-%             end
-%         end
-%         x= ifft(Y_temp,NFFT);
-%         if temp < psnr(s,x(1:Size(2)),255)
-%             temp=psnr(s,x(1:Size(2)),255);
-%             MAX=paramteter;
-%         end    
-%     end
-    %--------- Eliminate noise by power select---------------------
-%     for i=1:Ysize(2)
-%         if abs(Y(i)) < (5*Length/2);%MAX %(1*Length/2)
-%             Y(i) = 0;
-%         end
-%     end
-    %--------- Eliminate noise by Frequency select---------------------
-%     for i=1:255
-%         if i*1.9531 > 20      % frequency value
-%             Y(i) = 0;
-%         else
-%             Y(i)=Y(i)*0.999;
-%         end    
-%     end
-
-
-
-%     threshold  = mean(P1(1:NFFT/2))
-%     for i=1:Ysize(2)
-%         if abs(Y(i))<(threshold*Length/2)
-%           Y(i)=Y(i)*0.99;  
-%         end  
-%     end
+    FFT_output_size=size(FFT_output,2);
+    Optimised_threshold=0;
+    performance=0;
     
-%    sprintf('Section %d Mean= %f',segment, mean(P1(1:NFFT/2))) 
-%     for paramteter=1:50
-%         Y_temp=Y;
-%         threshold = paramteter/10;
-%        for i=1:Ysize(2)
-%             if abs(Y_temp(i))<(threshold*Length/2)
-%             Y_temp(i)=Y_temp(i)*0.99;  
-%             end  
-%        end
-%        
-%        x= ifft(Y_temp,NFFT);
-%        Excel(segment,paramteter)=psnr(s,x(1:Size(2)),255);
-%        %Excel(1,paramteter)=threshold;
-%        if temp < psnr(s,x(1:Size(2)),255)
-%             temp=psnr(s,x(1:Size(2)),255);
-%             MAX=threshold;
-%        end    
-%     end
-
-performance=0;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 for parameter=1:100
-    tempY=Y;
+    FFT_temp=FFT_output;
     threshold=parameter/100;
     %MAX=mean(n2);
-    sprintf('Section %d MAX= %f',segment, MAX);
-    for i=1:Ysize(2)
+    sprintf('Section %d MAX= %f',segment, Optimised_threshold);
+    for i=1:FFT_output_size
         if(rem(fix(i*(Fs/NFFT)*1000),2)==1)
-            if abs(tempY(i))<(threshold*Length/2)
-            tempY(i)=0;  
+            if abs(FFT_temp(i))<(threshold*Length/2)
+            FFT_temp(i)=0;  
             else
-            %tempY(i)=tempY(i)*0.99;  
+            %FFT_temp(i)=FFT_temp(i)*0.99;  
             end  
         end    
     end   
     
-    x= ifft(tempY,NFFT);
-    %Excel(segment,parameter)=psnr(s,x(1:Size(2)),255);
-    if (performance < psnr(s,x(1:Size(2)),255))
-        performance=psnr(s,x(1:Size(2)),255);
-        MAX=threshold;
+    iFFT_output= ifft(FFT_temp,NFFT);
+    %Excel(segment,parameter)=psnr(s,iFFT_output(1:Size),255);
+    if (performance < psnr(Original_segment_signal,iFFT_output(1:Size),255))
+        performance=psnr(Original_segment_signal,iFFT_output(1:Size),255);
+        Optimised_threshold=threshold;
     end    
 end   
-    MAX
-    for i=1:Ysize(2)
+
+    for i=1:FFT_output_size
         if(rem(fix(i*(Fs/NFFT)*1000),2)==1)
-            if abs(Y(i))<(MAX*Length/2)
-            Y(i)=0;
+            if abs(FFT_output(i))<(Optimised_threshold*Length/2)
+            FFT_output(i)=0;
             else
-            %Y(i)=Y(i)*0.99;
+            %FFT_output(i)=FFT_output(i)*0.99;
             end 
         end    
     end   
-
-   % sprintf('Dnoise Signal   PSNR=%f', psnr(s,x(1:Size(2)),255))
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%     threshold=200;
-%     for i=1:256
-%        if (i*(Fs/NFFT)> 200)&(i*(Fs/NFFT)<= 500)&(rem(fix(i*(Fs/NFFT)*1000),2)==1)
-%            Y(i)=0;
-%        else
-%            Y(i)=Y(i)*0.999;
-%        end 
-%     end
-    %--------- Eliminate noise by Noise power distribution-------------
-  %Y=Y*0.9996;
     
-% %% -------------(Denoise) Frequency Domain---------------------
-    P2 = abs(Y/Length);
-    P1 = P2(1:NFFT/2+1);
-    P1(2:end-1) = 2*P1(2:end-1);
-    plot(0:(1/NFFT):(1/2-1/NFFT),P1(1:NFFT/2),'G');
+%% ---------(Denoise) Frequency Domain---------
+    FFT_Temp_2 = abs(FFT_output/Length);
+    FFT_Temp_1 = FFT_Temp_2(1:NFFT/2+1);
+    FFT_Temp_1(2:end-1) = 2*FFT_Temp_1(2:end-1);
+    plot(0:(1/NFFT):(1/2-1/NFFT),FFT_Temp_1(1:NFFT/2),'G');
     hold off
     legend('Original','Noisy',' Denoise')
     
-%% ----------------IFFT----------------------------
-x= ifft(Y,NFFT);
+%% ---------IFFT---------
+iFFT_output= ifft(FFT_output,NFFT);
 subplot(4,1,4);
-plot(t(1:Size(2)),x(1:Size(2)));
-sprintf('Max parameter is %d',(MAX*2/Length));
+plot(t(1:Size),iFFT_output(1:Size));
+sprintf('Max parameter is %d',(Optimised_threshold*2/Length));
 xlabel('time (s)'); 
 ylabel('voltage (mV)');
-txt=sprintf('Dnoise Signal   PSNR=%f dB', psnr(s,x(1:Size(2)),255));
+txt=sprintf('Dnoise Signal   PSNR=%f dB', psnr(Original_segment_signal,iFFT_output(1:Size),255));
 title(txt);   
-New(section_length(segment)+1:section_length(segment+1))=x(1:Size(2));
+New(section_length(segment)+1:section_length(segment+1))=iFFT_output(1:Size);
 end 
+
+%% ----------------Plot the Recovered Signal----------------------------
 figure(6);
 subplot(3,1,1)
 plot(t(1:2296),signal);
@@ -196,10 +125,10 @@ plot(t(1:2296),signal);
     title('Original Signal ');
     
 subplot(3,1,2)
-plot(t(1:2296),y1);
+plot(t(1:2296),Mix);
     xlabel('time (s)'); 
     ylabel('voltage (mV)');
-    txt=sprintf('Noise & Singal  PSNR=%f dB', psnr(signal,y1,255));
+    txt=sprintf('Noise & Singal  PSNR=%f dB', psnr(signal,Mix,255));
     title(txt); 
     
 subplot(3,1,3)
